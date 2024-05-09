2e9c9ebb719c37602431796c48c8a4e9a45cacf0
HMZeeshanLatif <mzeeshan@digital-dividend.com>
2024-05-09 12:23:53 +0500

    Passing pram function as attribute

--- src/components/HeroSection/index.vue
+++ src/components/HeroSection/index.vue
@@ -1,25 +1,25 @@
 <script setup>
-const numbers = [-3,-2,-1, 2, 3, 4, 5, 6];
+const numbers = [-3, -2, -1, 2, 3, 4, 5, 6];
 const multiple = {
   id: "testId",
   class: "testClass",
   style: "background-color:aqua",
 };
-  let test="Properties";
-  const reversedTest = test.split('').reverse().join('');
-  console.log(test.split('')[0])
+let test = "Properties";
+const reversedTest = test.split("").reverse().join("");
+
+let funTest = (first, second) => {
+  return first + "" + second;
+};
 </script>
 <template>
-
-  <h1>
-         {{
-              test.split('').reverse().join()
-         }}
+  <h1 :id="funTest('hello',2)">
+    {{ test.split("").reverse().join() }}
+    {{ test == reversedTest ? "ok" : "none" }}
   </h1>
   <h1 v-bind="multiple" v-for="(item, index) in numbers" :key="index">
-    {{ item>=+3?item:"" }}
+    {{ item >= +3 ? item : "" }}
     {{ index }}
-  
   </h1>
 </template>
 
