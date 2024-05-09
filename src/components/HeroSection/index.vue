a137a26d14a0d50ae9449b3d9d52748b49e1ac8e
HMZeeshanLatif <mzeeshan@digital-dividend.com>
2024-05-09 11:13:09 +0500

    Array Looping

--- src/components/HeroSection/index.vue
+++ src/components/HeroSection/index.vue
@@ -1,9 +1,10 @@
-<script setup>
-
-</script>
+<script setup></script>
 <template>
- <h1>I am Hero</h1>   
+  <h1>
+    <span v-for="(item, index) in [1, 2, 3, 4, 5, 6, 7, 8, 9]" :key="index">
+      {{ item }}
+    </span>
+  </h1>
 </template>
-<style scoped>
 
-</style>
+<style scoped></style>
