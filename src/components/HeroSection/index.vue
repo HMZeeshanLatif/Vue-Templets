276b27ba175622e083ace4644d8b672a15b65508
HMZeeshanLatif <mzeeshan@digital-dividend.com>
2024-05-09 13:21:15 +0500

    Use of ref a
    ref is like hook in vue.js alternate of useState hook of react

--- src/components/HeroSection/index.vue
+++ src/components/HeroSection/index.vue
@@ -1,19 +1,25 @@
 <script setup>
+import {ref } from "vue";
+const count=ref("Hello")
 const numbers = [-3, -2, -1, 2, 3, 4, 5, 6];
 const multiple = {
   id: "testId",
   class: "testClass",
   style: "background-color:aqua",
 };
+const increment = () => {
+  count.value="World";
+};
 let test = "Properties";
 const reversedTest = test.split("").reverse().join("");
-
 let funTest = (first, second) => {
   return first + "" + second;
 };
 </script>
 <template>
-  <h1 :id="funTest('hello',2)">
+   <p>{{ count }}</p>
+    <button @click="increment">increment</button>
+  <h1 :id="funTest('hello', 2)">
     {{ test.split("").reverse().join() }}
     {{ test == reversedTest ? "ok" : "none" }}
   </h1>
