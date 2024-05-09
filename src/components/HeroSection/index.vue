8ca8d31e282ac680725ab8feb7680e1d831dc710
HMZeeshanLatif <mzeeshan@digital-dividend.com>
2024-05-09 16:50:26 +0500

    use of reactive property

--- src/components/HeroSection/index.vue
+++ src/components/HeroSection/index.vue
@@ -1,32 +1,21 @@
 <script setup>
-import {ref } from "vue";
-const count=ref("Hello")
-const numbers = [-3, -2, -1, 2, 3, 4, 5, 6];
-const multiple = {
-  id: "testId",
-  class: "testClass",
-  style: "background-color:aqua",
-};
-const increment = () => {
-  count.value="World";
-};
-let test = "Properties";
-const reversedTest = test.split("").reverse().join("");
-let funTest = (first, second) => {
-  return first + "" + second;
-};
+import {reactive} from "vue";
+ let count=reactive({
+  a:50,
+  b:20,
+  c:0
+ })
+
+let fun=()=>{
+  count.c= count.a+count.b;
+  
+}
+
+
 </script>
 <template>
-   <p>{{ count }}</p>
-    <button @click="increment">increment</button>
-  <h1 :id="funTest('hello', 2)">
-    {{ test.split("").reverse().join() }}
-    {{ test == reversedTest ? "ok" : "none" }}
-  </h1>
-  <h1 v-bind="multiple" v-for="(item, index) in numbers" :key="index">
-    {{ item >= +3 ? item : "" }}
-    {{ index }}
-  </h1>
+   <h1>{{count.a + count.c}}</h1>
+   <button  @click="fun()">click</button>
 </template>
 
 <style scoped></style>
