f90380891b5df29c4e5519a62d1988ab157025a8
HMZeeshanLatif <mzeeshan@digital-dividend.com>
2024-05-13 16:03:17 +0500

    Computed

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <script setup>
-import {ref} from 'vue';
-const count=ref(0)
-const incr=()=>{
-  count.value++;
-}
-const dec=()=>{
-  count.value--;
-}
+import { ref, computed } from "vue";
 
+const number = ref(5);
+const ComputedNumber = computed(() => {
+  return number.value *  number.value
+});
 </script>
 
 <template>
-   <h1>{{count}}</h1>
-   <button @click="incr()">Incriment</button>
-   <button @click="dec()">Incriment</button>
+  <h1>{{ number }}</h1>
+  <h1>{{ComputedNumber}}</h1>
 </template>
 
 <style scoped></style>
