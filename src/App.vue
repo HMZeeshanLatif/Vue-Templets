a04844808c05fd529b90f95ec845678b63966819
MuhammadZeshanLatif <zeeshanmuhammad42201@gmail.com>
2024-05-14 11:54:17 +0500

    form submession
    Add  @submit.prevent="alert('Form submitted')"
    to prevent form from submession

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <script setup>
 import { ref } from "vue";
 
-const formDatas = ref({ userName: "" });
+const formDatas = ref({ userName: "", email: "", password: "" });
 </script>
 
 <template>
-  <input type="text" name="" id="" placeholder="Enter any text Here" v-model="formDatas.userName">
-  <h1>{{formDatas.userName}}</h1>
+  <form @submit.prevent="alert('Form submitted')">
+    <input
+      type="text"
+      name=""
+      id=""
+      placeholder="Enter any text Here"
+      v-model="formDatas.userName"
+    />
+    <input
+      type="email"
+      name=""
+      id=""
+      placeholder="Enter any text Here"
+      v-model="formDatas.email"
+    />
+    <input
+      type="password"
+      name=""
+      id=""
+      placeholder="Enter any text Here"
+      v-model="formDatas.password"
+    />
+    <button>Submit</button>
+  </form>
+  <h1>{{ formDatas.userName }}</h1>
+  <h1>{{ formDatas.email }}</h1>
+  <h1>{{ formDatas.password }}</h1>
 </template>
 
 <style scoped></style>
