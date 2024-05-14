a7b49d3fa4f4db915180f5df4c4b3dde90b61667
MuhammadZeshanLatif <zeeshanmuhammad42201@gmail.com>
2024-05-14 12:26:10 +0500

    Dynamic props

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,14 @@
 <script setup>
 import { ref } from "vue";
 import HeroSection from './components/HeroSection/index.vue'
-
-const formDatas = ref({ userName: "", email: "", password: "" });
+const firstName=ref("Zeeshan")
+const lastname=ref("Latif")
 </script>
 
 <template>
-  <form @submit.prevent="alert('Form submitted')">
-    <input
-      type="text"
-      name=""
-      id=""
-      placeholder="Enter any text Here"
-      v-model="formDatas.userName"
-    />
-    <input
-      type="email"
-      name=""
-      id=""
-      placeholder="Enter any text Here"
-      v-model="formDatas.email"
-    />
-    <input
-      type="password"
-      name=""
-      id=""
-      placeholder="Enter any text Here"
-      v-model="formDatas.password"
-    />
-    <button>Submit</button>
-  </form>
-  <h1>{{ formDatas.userName }}</h1>
-  <h1>{{ formDatas.email }}</h1>
-  <h1>{{ formDatas.password }}</h1>
-  <HeroSection name="Zeeshan"/>
-  <HeroSection name="farhan"/>
+ 
+  <HeroSection :first-Name="firstName" :last-name="lastname"/>
+  <HeroSection />
 </template>
 
 <style scoped></style>
--- src/components/HeroSection/index.vue
+++ src/components/HeroSection/index.vue
@@ -1,11 +1,13 @@
 <script setup>
- const propos= defineProps(["name"])
+ const propos= defineProps(["firstName","lastName"])
 
 </script>
 
 <template>
 
-<h1>{{ propos.name }}</h1>
+<h1>{{ propos.firstName }}</h1>
+<h1>{{propos.lastName}}</h1>
+
 </template>
 
 <style scoped>
